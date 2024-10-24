9c4f6f1f1ef13a466be63a0c23c10c17e04850ad
siddartha <varunsiddartha82786@gmail.com>
2024-10-24 14:48:03 +0530

    changes for new network

--- peltier_system/esp32-system/include/secrets.h
+++ peltier_system/esp32-system/include/secrets.h
@@ -2,9 +2,13 @@
 #define SECRETS_H
 
 // WiFi credentials
-const char* ssid = "Home-Sweet-Home";
-const char* password = "EAVVballa@82786";
-const char* serverUrl = "http://192.168.31.143:5000";  // Raspberry Pi IP address
+// const char* ssid = "Home-Sweet-Home";
+// const char* password = "EAVVballa@82786";
+// const char* serverUrl = "http://192.168.31.143:5000";  // Raspberry Pi IP address
+
+const char* ssid = "RpiServer-007";
+const char* password = "Cooperstation";
+const char* serverUrl = "http://192.168.1.104:5000";  // Raspberry Pi IP address
 
 
 #endif
--- peltier_system/rpi_system/ESP32Ops.py
+++ peltier_system/rpi_system/ESP32Ops.py
@@ -7,8 +7,9 @@ from thingsSpeak import *
 
 
 
-ESP32_IP = "http://192.168.31.172:8080"  # Replace with ESP32 IP address
+#ESP32_IP = "http://192.168.31.172:8080"  # Replace with ESP32 IP address
 
+ESP32_IP = "http://192.168.1.107:8080"  # Replace with ESP32 IP address
 ts_counter = 0; # counter for sending data to things speak
 
 #collect and send data
